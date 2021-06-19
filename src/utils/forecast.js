741e9fe964f746b514e24640c1c7b67541a2600f
Ananya Sharma <ananyasubodh8@gmail.com>
2021-06-19 20:54:25 +0530

    Retify coordinate issue(weatherstack api had)

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('request')
 //defining
 const foreCastfuncWithCallback = (long, lati, callback) => {
   const access_key = '22a69dc6407ce58e1c665f382f4638c6'
-  const forecastURL = `http://api.weatherstack.com/current?access_key=${access_key}&query=${long},${lati}`
+  const forecastURL = `http://api.weatherstack.com/current?access_key=${access_key}&query=${lati},${long}`
   request({url: forecastURL, json: true}, (error, response) => {
     if (error){
       callback('Unable to connect', undefined)
